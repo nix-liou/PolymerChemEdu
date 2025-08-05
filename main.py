faf826a02dced107b94837b749a81f1746d53b91
nix-liou <btinkle@calpoly.edu>
2025-08-05 16:30:08 -0700

    Added basic generation

--- main.py
+++ main.py
@@ -6,13 +6,13 @@ from typing import Optional
 @dataclass
 class Monomer:
     """
-    Individual monomer data. A list of available unreacted functional groups, represented by numbers to indcate type of functionality.
+    Individual monomer data. A list of available unreacted functional groups, represented by numbers to indicate type of functionality.
     Bonds indicate already bonded other monomers
     Position indicates location in the simulation for display/simulation purposes
     """
-    functionalities: list[int]
-    bonds: list['Monomer']
-    position: tuple[float, float]
+    functionalities: list[int] = list[1, 1]
+    bonds: list[Optional['Monomer']] = list[None]
+    position: tuple[float, float] = tuple[0, 0]
 
 @dataclass
 class SimulationState:
@@ -20,29 +20,33 @@ class SimulationState:
     Dataclass holding current simulation state, effectively as a list of graphs, each graph being a polymer chain. Previous and future states are linked to allow for traversal forwards and backwars in simulation
     """
     molecules: list[Monomer]
+    root: Monomer = None
     prev_state: Optional['SimulationState'] = None
     next_state: Optional['SimulationState'] = None
 
 
 
-
 class Simulation:
-    def __init__(self, monomer_makeup: list[tuple[int, list[int]]], polymerization_type, ):
+    def __init__(self, monomer_makeup: list[int], polymerization_type, ) -> None:
         """
         :param monomer_makeup: list of types of monomers, each a tuple of parameters for the monomer makeup. (monomer count, list functionalities present, average functionality, etc)
         :param polymerization_type: chain vs step growth
         """
-        state: SimulationState = SimulationState(self.gen_starting_monomers(monomer_makeup))
+        state = SimulationState(root=self.gen_starting_monomers(monomer_makeup))
 
-        pass
+        return None
 
-    def gen_starting_monomers(self, monomer_makeup):
+    def gen_starting_monomers(self, monomer_makeup: list[int]) -> SimulationState:
         """
         Starting monomer makeup to be created and distributed. Will calculate initial positions of all monomers
         :param monomer_makeup:
         :return:
         """
-        pass
+        new_state = SimulationState()
+        for i in range(monomer_makeup[0]):
+            new_state.molecules.append(Monomer())
+
+        return new_state
 
     def step_forward(self):
         """
